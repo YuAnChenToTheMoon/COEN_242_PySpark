5eef012335c9b4a043be132d5352dc1d5236d98d
ChenYuAn <popandthebestboy@gmail.com>
2022-05-29 13:46:53 -0700

    predit isFraud

--- Linear_Regression_Pyspark.py
+++ Linear_Regression_Pyspark.py
@@ -1,5 +1,6 @@
 #Start a new Spark Session
 from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
 
 # App named 'Cruise'
 spark = SparkSession.builder.appName('cruise').getOrCreate()
@@ -42,9 +43,19 @@ df.describe().show()
 
 #Convert string categorical values to integer categorical values
 from pyspark.ml.feature import StringIndexer
-indexer = StringIndexer(inputCol="creditLimit", outputCol = "credit_limit_output")
-indexed = indexer.fit(df).transform(df)
-indexed.head(5)
+# indexer = StringIndexer(inputCol="merchantName", outputCol = "merchantNameNum")
+# indexed = indexer.setHandleInvalid("skip").fit(df).transform(df)
+# indexer = StringIndexer(inputCol="acqCountry", outputCol = "acqCountryNum")
+# indexed = indexer.setHandleInvalid("skip").fit(indexed).transform(indexed)
+# indexer = StringIndexer(inputCol="merchantCountryCode", outputCol = "merchantCountryCodeNum")
+# indexed = indexer.setHandleInvalid("skip").fit(indexed).transform(indexed)
+# indexer = StringIndexer(inputCol="merchantCategoryCode", outputCol = "merchantCategoryNum")
+# indexed = indexer.setHandleInvalid("skip").fit(indexed).transform(indexed)
+# indexer = StringIndexer(inputCol="transactionType", outputCol = "transactionTypeNum")
+# indexed = indexer.setHandleInvalid("skip").fit(indexed).transform(indexed)
+indexed = df.withColumn("cardPresent", df.cardPresent.cast('int'))
+indexed = indexed.withColumn("isFraud", indexed.isFraud.cast('int'))
+indexed = indexed.withColumn("expirationDateKeyInMatch", indexed.isFraud.cast('int'))
 
 
 # In[24]:
@@ -70,8 +81,8 @@ indexed.columns
 
 # Create assembler object to include only relevant columns 
 assembler = VectorAssembler(
-inputCols=["creditLimit", "availableMoney", "transactionAmount"],
-outputCol="Features")
+inputCols=["accountNumber", "customerId", "creditLimit", "transactionAmount", "cardCVV", "enteredCVV", "cardLast4Digits", "currentBalance", "cardPresent", "expirationDateKeyInMatch"],
+outputCol="features")
 
 
 # In[29]:
@@ -83,79 +94,103 @@ output = assembler.transform(indexed)
 # In[30]:
 
 
-output.select("features","creditLimit").show()
+# output.select("features","availableMoney").show()
 
 
 # In[31]:
 
-
-final_data = output.select("features","creditLimit")
-
+final_data = output.select("features", "isFraud")
+valid_data = final_data.filter(df.isFraud == 0)
+valid_data = valid_data.orderBy(F.rand()).limit(12417)
+invalid_data = final_data.filter(df.isFraud == 1)
+final_data = valid_data.union(invalid_data)
 
 # In[32]:
 
 
 #Split the train and test data into 70/30 ratio
-train_data,test_data = final_data.randomSplit([0.7,0.3])
+train_data,test_data = final_data.randomSplit([0.70,0.30])
 
 
 # In[33]:
 
 
 from pyspark.ml.regression import LinearRegression
+from pyspark.ml.classification import LogisticRegression
 
 
 # In[34]:
 
 
 #Training the linear model
-lr = LinearRegression(labelCol='creditLimit')
+# lr = LinearRegression(labelCol="availableMoney")
+lr = LogisticRegression(labelCol="isFraud")
 
 
-# In[35]:
+# # In[35]:
 
 
 
 lrModel = lr.fit(train_data)
 
 
-# In[39]:
+# # In[39]:
 
 
 
-print("Coefficients: {} Intercept: {}".format(lrModel.coefficients,lrModel.intercept))
+# print("Coefficients: {} Intercept: {}".format(lrModel.coefficients,lrModel.intercept))
 
 
-# In[40]:
+# # In[40]:
 
 
-#Evaluate the results with the test data
+# #Evaluate the results with the test data
 test_results = lrModel.evaluate(test_data)
+test_result = test_results.predictions
+test_result.filter(test_result["isFraud"] == 1).show(5)
+
+tp = test_result[(test_result.isFraud == 1) & (test_result.prediction == 1)].count()
+tn = test_result[(test_result.isFraud == 0) & (test_result.prediction == 1)].count()
+fp = test_result[(test_result.isFraud == 0) & (test_result.prediction == 1)].count()
+fn = test_result[(test_result.isFraud == 1) & (test_result.prediction == 0)].count()
+accuracy = float(tp + tn) / (tp + tn + fp + fn)
+precision = float(tp) / (tp + fp)
+recall = float(tp) / (tp + fn)
+f1 = 2*(recall*precision)/(recall+precision)
+final_data.groupBy("isFraud").count().show()
+print("tp:", tp)
+print("tn:", tn)
+print("fp:", fp)
+print("fp:", fp)
+print("accuracy:", accuracy)
+print("precision:", precision)
+print("recall:", recall)
+print("F1 score:", f1)
+
+# # In[41]:
 
 
-# In[41]:
+# print("RMSE: {}".format(test_results.rootMeanSquaredError))
+# print("MSE: {}".format(test_results.meanSquaredError))
+# print("R2: {}".format(test_results.r2))
 
 
-print("RMSE: {}".format(test_results.rootMeanSquaredError))
-print("MSE: {}".format(test_results.meanSquaredError))
-print("R2: {}".format(test_results.r2))
+
+# # In[42]:
 
 
-# In[42]:
+# from pyspark.sql.functions import corr
 
 
-from pyspark.sql.functions import corr
+# # In[43]:
 
 
-# In[43]:
+# #Checking for correlations to explain high R2 values
+# df.select(corr('creditLimit','availableMoney')).show()
 
 
-#Checking for correlations to explain high R2 values
-df.select(corr('creditLimit','availableMoney')).show()
+# # In[44]:
 
 
-# In[44]:
-
-
-df.select(corr('creditLimit','transactionAmount')).show()
+# df.select(corr('creditLimit','transactionAmount')).show()
 
